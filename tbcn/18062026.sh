c44e9994562dba82c92657640c6dac43fa048fe9
Soundtrack <58030730+c9BM@users.noreply.github.com>
2026-06-17 22:52:20 +0700

    Create 18062026

---
+++ tbcn/18062026.sh
@@ -0,0 +1,5 @@
+Ngày 17/6, Ủy ban Kiểm tra Thành ủy Đà Nẵng đã có thông cáo về kỳ họp thứ 11 nhiệm kỳ 2025-2030, dưới sự chủ trì của ông Trần Thắng Lợi, Chủ nhiệm Ủy ban Kiểm tra Thành ủy.
+
+Ủy ban Kiểm tra Thành ủy đã xem xét, kết luận nhiều nội dung thuộc thẩm quyền, trong đó quyết định thi hành kỷ luật bằng hình thức khai trừ ra khỏi Đảng đối với ông Nguyễn Quang Hoàn, nguyên Bí thư Chi bộ, nguyên Giám đốc Kho bạc Nhà nước huyện Nam Giang, tỉnh Quảng Nam (cũ).
+
+Thông tin chi tiết: https://dantri.com.vn/thoi-su/nguyen-giam-doc-kho-bac-bi-khai-tru-khoi-dang-20260617192859453.htm
